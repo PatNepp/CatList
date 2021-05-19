29f9a70d55b70b4014bed700aa5d6b53f57e3700
Patrick Nepp <ptnepp@gmail.com>
2021-05-18 22:25:18 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route } from 'react-router';
+import CatList from './CatList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	static defaultProps = {
+		cats: [
+			{
+				name: 'Whiskers',
+				age: 5,
+				src:
+					'https://images.unsplash.com/photo-1572360620839-a02312482daf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8NXx8fGVufDB8fHx8&w=1000&q=80',
+				facts: [
+					'Whiskers loves eating popcorn.',
+					'Whiskers is a sweet cat.',
+					'Whiskers wants to cuddle with you!'
+				]
+			},
+			{
+				name: 'Hazel',
+				age: 3,
+				src: 'https://c.files.bbci.co.uk/12A9B/production/_111434467_gettyimages-1143489763.jpg',
+				facts: [ 'Hazel has soooo much energy!', 'Hazel is highly intelligent.', 'Hazel loves people.' ]
+			},
+			{
+				name: 'Tubby',
+				age: 6,
+				src:
+					'https://www.inquirer.com/resizer/33-eBuWFn_ZnRwxriP-n2OdOLgw=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/J4QDHA4WPZHJJJKAUWMI6HGL6A.jpg',
+				facts: [ 'Tubby is not the brightest cat', 'Tubby does not like exercise.', 'Tubby loves eating food.' ]
+			}
+		]
+	};
+	render() {
+		return <Route path="/cats" render={() => <CatList cats={this.props.cats} />} />;
+	}
 }
 
 export default App;
---
+++ src/CatList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './CatList.css';
+
+class CatList extends Component {
+	render() {
+		return (
+			<div className="CatList">
+				<h1 className="display-1 text-center">Cat List</h1>
+				<div className="container">
+					<div className="row">
+						{this.props.cats.map((c) => (
+							<div className="Cat col-lg-4 text-center" key="c.name">
+								<img src={c.src} alt={c.name} />
+								<h3>{c.name}</h3>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default CatList;
