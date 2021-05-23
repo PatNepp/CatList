e6604ebfb9b7b993b572132b745b62ba016d32b2
Patrick Nepp <ptnepp@gmail.com>
2021-05-23 09:11:25 -0500

    fixed navbar hamburger bug

--- src/Navbar.js
+++ src/Navbar.js
@@ -18,15 +18,15 @@ class Navbar extends Component {
 				<button
 					className="navbar-toggler"
 					type="button"
-					data-toggle="collapse"
-					data-target="#navbarNav"
-					aria-controls="navbarNav"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarSupportedContent"
+					aria-controls="navbarSupportedContent'"
 					aria-expanded="false"
 					aria-label="Toggle navigation"
 				>
 					<span className="navbar-toggler-icon" />
 				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav">
 						<li className="nav-item">
 							<NavLink exact to="/cats" className="nav-link">
